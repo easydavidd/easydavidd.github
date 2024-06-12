4c84bd3692da00556260020cfd1e8a1dd2ef26b5
easydavidd <168437850+easydavidd@users.noreply.github.com>
2024-06-12 01:04:28 +0100

    Add files via upload

---
+++ femme.js
@@ -0,0 +1,6 @@
+const loupe = document.querySelector('.loupe');
+const barreRecherche = document.querySelector('.barreRecherche');
+
+loupe.addEventListener('click', () => {
+    document.querySelector(".barreRecherche").style.display = "flex"
+});
